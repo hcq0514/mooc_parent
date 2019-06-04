03c1c9734f88cf9c79c3305f0b5ba9809e1b8a3b
hcq <hcq0514@163.com>
2019-06-04 20:18:51 +0800

    Course application.yml

--- mooc_course/src/main/java/com/mooc/course/CourseApplication.java
+++ mooc_course/src/main/java/com/mooc/course/CourseApplication.java
@@ -3,10 +3,11 @@ package com.mooc.course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
-@SpringBootApplication
 @EnableEurekaClient
+@SpringBootApplication(scanBasePackages = "com.mooc.*")
+@EnableSwagger2
 public class CourseApplication {
 
     public static void main(String[] args) {
