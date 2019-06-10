7fd6d4aae329baa1da95d75d13c471e9042cdfc1
hcq <hcq0514@163.com>
2019-06-10 13:56:56 +0800

    Fix consume bug

--- mooc_mq_consume/src/main/java/com/mooc/mq/consume/cms/client/CmsPageClient.java
+++ mooc_mq_consume/src/main/java/com/mooc/mq/consume/cms/client/CmsPageClient.java
@@ -3,7 +3,7 @@ package com.mooc.mq.consume.cms.client;
 import com.mooc.api.cms.CmsPageControllerApi;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient("mooc-manager-cms-service")
+@FeignClient("manager-cms-service")
 public interface CmsPageClient extends CmsPageControllerApi{
 
 }
--- mooc_mq_consume/src/main/java/com/mooc/mq/consume/cms/client/CmsSiteClient.java
+++ mooc_mq_consume/src/main/java/com/mooc/mq/consume/cms/client/CmsSiteClient.java
@@ -3,7 +3,7 @@ package com.mooc.mq.consume.cms.client;
 import com.mooc.api.cms.CmsSiteControllerApi;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient("mooc-manager-cms-service")
+@FeignClient("manager-cms-service")
 public interface CmsSiteClient extends CmsSiteControllerApi {
 
 }
